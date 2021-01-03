590dbabe5dc63c3473b6785017cd3dfbd2de04ad
Vaishnavi Nilkanth Muley <54570348+VaishnaviMuley19@users.noreply.github.com>
2021-01-03 23:06:32 +0530

    Update 2_X_raised_to_power_N.py

--- Lecture_2_Introduction_to_Python/2_X_raised_to_power_N.py
+++ Lecture_2_Introduction_to_Python/2_X_raised_to_power_N.py
@@ -1,4 +1,15 @@
 """ You are given two integers: X and N. You have to calculate X raised to power N and print it.
+
+Input format:
+The first line of input contains an integer X (1 <= X <= 100)
+The second line of input contains an integer N (1 <= N <= 10) 
+
+Constraints:
+Time Limit: 1 second
+
+Output format:
+The first and only line of output contains the result. 
+
 Sample Input:
 10
 4
